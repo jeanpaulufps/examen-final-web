23294b72dc3f5f8ad4a899c8c893c9318de0dca2
JeanP78 <jeanpaul04042003@gmail.com>
2024-12-06 08:15:07 -0500

    paginas añadidas

--- examen/votacion/urls.py
+++ examen/votacion/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import VotarOpcion, VerResultados
+from votacion import views
 
 urlpatterns = [
-    path('votar/<int:opcion_id>/', VotarOpcion.as_view(), name='votar_opcion'),
-    path('resultados/<int:encuesta_id>/', VerResultados.as_view(), name='ver_resultados'),
+    path('api/votar/<int:opcion_id>/', views.VotarOpcion.as_view(), name='votar_opcion'),
+    path('api/resultados/<int:encuesta_id>/', views.VerResultados.as_view(), name='ver_resultados'),
+    path('', views.ListaEncuestasView.as_view(), name='lista_encuestas'),
+    path('encuesta/<int:pk>/', views.DetalleEncuestaView.as_view(), name='detalle_encuesta'),
+    path('encuesta/<int:pk>/votar/', views.votar, name='votar'),
 ]
--- examen/votacion/views.py
+++ examen/votacion/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from .models import Encuesta, Opcion
 from .serializers import EncuestaSerializer
+from django.views.generic import ListView, DetailView
+from django.http import JsonResponse
 
 class VotarOpcion(APIView):
     def post(self, request, opcion_id):
@@ -17,3 +19,28 @@ class VerResultados(APIView):
         encuesta = get_object_or_404(Encuesta, id=encuesta_id)
         serializer = EncuestaSerializer(encuesta)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView
+from .models import Encuesta, Opcion
+import random
+
+class ListaEncuestasView(ListView):
+    model = Encuesta
+    template_name = 'lista_encuestas.html'
+    context_object_name = 'encuestas'
+
+class DetalleEncuestaView(DetailView):
+    model = Encuesta
+    template_name = 'detalle_encuesta.html'
+    context_object_name = 'encuesta'
+
+def votar(request, pk):
+    if request.method == "POST":
+        opcion_id = request.POST.get('opcion')
+        opcion = get_object_or_404(Opcion, id=opcion_id)
+        opcion.numero_votos += 1
+        opcion.save()
+        return redirect('detalle_encuesta', pk=pk)
+    return JsonResponse({"error": "Método no permitido"}, status=405)
